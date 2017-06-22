0b32f1d17c96b3414acb54199df65fe91f615f66
chanpan <chanpan.nuttaphon@gmail.com>
2017-06-22 14:27:59 +0700

    dropdownList

--- backend/controllers/TestController.php
+++ backend/controllers/TestController.php
@@ -5,15 +5,15 @@ use Yii;
 use yii\web\Controller;
 class TestController extends Controller{
     public function actionIndex(){
-        return $this->render("index");
+        
+        $model = new \backend\models\Test();
+        $provinceList = \backend\models\Province::find()->all();
+        return $this->render("index",[
+            "model"=>$model,
+            "provinceList"=>$provinceList
+        ]);    
+        
     }
     
-    public function actionCal(){
-        $num1 = 10;
-        $num2 = 20;
-        $sum = $num1+$num2;
-        return $this->render("cal",[
-            'sum'=>$sum
-        ]);
-    }
+    
 }
--- backend/views/test/index.php
+++ backend/views/test/index.php
@@ -1,7 +1,16 @@
 <?php 
+    use yii\bootstrap\ActiveForm;
     use \yii\helpers\Html;     
+    use yii\helpers\ArrayHelper;
 ?>
 
-<?php $form = yii\bootstrap\ActiveForm::begin();?>
+<?php $form = ActiveForm::begin();?>
+    
+    <?= $form->field($model, "province")->dropDownList(
+        ArrayHelper::map($provinceList,'PROVINCE_ID','PROVINCE_NAME'),[
+            "prompt"=>"เลือกจังหวัด"
+        ]
+    )?>
 
+    <?= Html::submitButton("save",['class'=>'btn btn-warning'])?>
 <?php ActiveForm::end();?>
